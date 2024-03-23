389deb4bda2713c1fa6d61d13ee82accd593aa60
supersaijaku <154524778+supersaijaku@users.noreply.github.com>
2024-03-23 21:48:13 +0900

    Update and rename app.html to app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// 配列のエクスポート
+export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// 定数のエクスポート
+export const MODULES_BECAME_STANDARD_YEAR = 2015;
+
+// クラスのエクスポート
+export class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
