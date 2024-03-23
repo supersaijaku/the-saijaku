9ed44e93221e2f65a82c5bdfacfdaf1bfbd60689
supersaijaku <154524778+supersaijaku@users.noreply.github.com>
2024-03-23 21:49:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-  // 配列のエクスポート
-export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-// 定数のエクスポート
-export const MODULES_BECAME_STANDARD_YEAR = 2015;
-
-// クラスのエクスポート
-export class User {
-  constructor(name) {
-    this.name = name;
-  }
+// 📁 say.js
+function sayHi(user) {
+  alert(`Hello, ${user}!`);
 }
-  
+
+function sayBye(user) {
+  alert(`Bye, ${user}!`);
+}
+
+export {sayHi, sayBye}; // エクスポートされた変数のリスト
